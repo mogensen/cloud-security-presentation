f0d6b34c7435be79453c4630c84314a7e743e3e8
Lasse Højgaard <lasse.hojgaard@gmail.com>
2022-01-04 11:19:51 +0100

    Disable authentication.
    - Add help text.
    - Pass any additional arugments to sonar-scanner-cli

--- 4-code/sonarqube/sonarqube.sh
+++ 4-code/sonarqube/sonarqube.sh
@@ -1,10 +1,25 @@
 #!/bin/bash
+
+if [[ "$1" = "-h" || "$1" = "--help" ]] ; then
+  echo "Usage: "
+  echo "$0 src-dir"
+  echo "Additional parameters can be passed to sonar-scanner-cli like this"
+  echo "$0 src-dir -Dsonar.java.binaries=./target/classes"
+  exit 0
+fi
+
 export PROJECT=$1
+shift # remove first argument - additional arguments are in "$@"
 
 echo "Starting Sonarqube.."
 
 # Run sonarqube - wait a few minutes until it has started
-docker run -d --name sonarqube -p 9000:9000 -p 9092:9092 sonarqube
+if [ -z $(docker ps | grep  sonarqube)]; then
+  docker run -d --name sonarqube -e SONAR_ES_BOOTSTRAP_CHECKS_DISABLE=true -e SONAR_FORCEAUTHENTICATION=false -p 9000:9000 -p 9092:9092 sonarqube
+else
+  echo "Sonarqube already started"
+fi
+
 
 echo "Waiting for Sonarqube.."
 until `docker logs sonarqube | grep -q "SonarQube is up"`
@@ -12,9 +27,10 @@ do
     echo -n "."
     sleep 0.2;
 done
+
 echo "Sonarqube is up. Scanning source code.."
 
 # Scan the source code
-docker run --rm -ti -v $(pwd)/$PROJECT:/usr/src --link sonarqube -e SONAR_HOST_URL="http://sonarqube:9000" sonarsource/sonar-scanner-cli -Dsonar.projectKey=$PROJECT
-
-echo See "http://localhost:9000/dashboard?id=$PROJECT"
+docker run --rm -ti -v $(pwd)/$PROJECT:/usr/src --link sonarqube -e SONAR_HOST_URL="http://sonarqube:9000"   sonarsource/sonar-scanner-cli -Dsonar.projectKey=$PROJECT "$@"
+echo
+echo "See http://localhost:9000/dashboard?id=$PROJECT"
